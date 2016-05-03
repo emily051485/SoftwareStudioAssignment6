0a3dd25128f45aa936639a3a7f12a5eded142c6d
jasonchang117 <jasonchang117@gmail.com>
2016-05-03 21:45:29 +0800

    add target value and hashmap for values

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,6 +1,9 @@
 package main.java;
 
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.HashMap;
+
 import processing.core.PApplet;
 
 /**
@@ -13,32 +16,51 @@ public class Character
 	private String name;
 	private MainApplet parent;
 	private ArrayList<Character> targets = new ArrayList<Character>();
+	private HashMap<String, Integer> values = new HashMap<String, Integer>();
+	@SuppressWarnings("unused")
+	private int order, r, g, b;
 	
-	
-	public Character(MainApplet parent, String name, float x, float y, float radius)
+	public Character(MainApplet parent, String name, int order, String color)
 	{
 		this.parent = parent;
 		this.name = name;
-		this.x = x;
-		this.y = y;
-		this.radius = radius;
+		this.order = order;
+		this.x = 10 + (order%10)*10;
+		this.y = 10 + order*10;
+		this.radius = 10;
+
+		r = Integer.valueOf( color.substring( 0, 1 ), 16 );
+        g = Integer.valueOf( color.substring( 2, 3 ), 16 );
+        b = Integer.valueOf( color.substring( 4, 5 ), 16 );
+        System.out.println(r + " " + g + " " + b);
 	}
 
 	public void display()
 	{
-		this.parent.fill(100,100,235);
-		this.parent.ellipse(x, y, 30, 30);
+		//this.parent.fill();
+		this.parent.ellipse(x, y, radius, radius);;
 		this.parent.fill(0);
 		this.parent.text(name, x+10, y+10);
 	}
 	
-	public void addTarget(Character target)
+	public void addTarget(Character target, int value)
 	{
+		values.put(target.getName(), value);
 		targets.add(target);
 	}
 	
 	public ArrayList<Character> getTargets()
 	{
 		return this.targets;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public int getvalue(Character tar)
+	{
+		return values.get(tar);
 	}
 }
